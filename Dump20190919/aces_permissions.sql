1450182b3c18a3ab67561aecb1cedba49c8fc0d7
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2019-09-19 10:48:59 -0400

    Database Dump

---
+++ Dump20190919/aces_permissions.sql
@@ -0,0 +1,69 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: aces
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissions` (
+  `idtime_window` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` time(4) DEFAULT NULL,
+  `end_time` time(4) DEFAULT NULL,
+  `monday` tinyint(1) DEFAULT NULL,
+  `tuesday` tinyint(1) DEFAULT NULL,
+  `wednesday` tinyint(1) DEFAULT NULL,
+  `thursday` tinyint(1) DEFAULT NULL,
+  `friday` tinyint(1) DEFAULT NULL,
+  `saturday` tinyint(1) DEFAULT NULL,
+  `sunday` tinyint(1) DEFAULT NULL,
+  `time_window_name` varchar(45) DEFAULT NULL,
+  `can_mod_users` tinyint(1) DEFAULT NULL,
+  `can_mod_system` tinyint(1) DEFAULT NULL,
+  `can_access_door1` tinyint(1) DEFAULT NULL,
+  `can_access_door2` tinyint(1) DEFAULT NULL,
+  `can_access_door3` tinyint(1) DEFAULT NULL,
+  `can_access_door4` tinyint(1) DEFAULT NULL,
+  `can_access_door5` tinyint(1) DEFAULT NULL,
+  `can_access_door6` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`idtime_window`),
+  UNIQUE KEY `idtime_window_UNIQUE` (`idtime_window`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'08:00:00.0000','17:00:00.0000',1,1,1,1,1,1,1,'workday',1,1,1,1,1,1,1,1),(2,'08:00:00.0000','08:00:00.0000',0,0,0,0,0,0,0,'noone',0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-19 10:45:48

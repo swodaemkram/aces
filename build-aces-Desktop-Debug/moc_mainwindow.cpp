482968c3169ce219a956bda965708d54624e41d8
Mark Meadows <42901465+swodaemkram@users.noreply.github.com>
2019-10-29 10:33:32 -0400

    VIM is now implamented

--- build-aces-Desktop-Debug/moc_mainwindow.cpp
+++ build-aces-Desktop-Debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[23];
-    char stringdata0[459];
+    QByteArrayData data[27];
+    char stringdata0[506];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -53,7 +53,11 @@ QT_MOC_LITERAL(18, 371, 21), // "Open_Lock2_SerialPort"
 QT_MOC_LITERAL(19, 393, 24), // "on_pushButton_16_clicked"
 QT_MOC_LITERAL(20, 418, 23), // "on_pushButton_5_clicked"
 QT_MOC_LITERAL(21, 442, 8), // "LogEvent"
-QT_MOC_LITERAL(22, 451, 7) // "EventID"
+QT_MOC_LITERAL(22, 451, 7), // "EventID"
+QT_MOC_LITERAL(23, 459, 15), // "open_vim_serial"
+QT_MOC_LITERAL(24, 475, 8), // "vim_comm"
+QT_MOC_LITERAL(25, 484, 10), // "lock2_comm"
+QT_MOC_LITERAL(26, 495, 10) // "lock1_comm"
 
     },
     "MainWindow\0on_pushButton_clicked\0\0"
@@ -69,7 +73,8 @@ QT_MOC_LITERAL(22, 451, 7) // "EventID"
     "DoorMonitorTimerSlot\0Open_Lock1_SerialPort\0"
     "Open_Lock2_SerialPort\0on_pushButton_16_clicked\0"
     "on_pushButton_5_clicked\0LogEvent\0"
-    "EventID"
+    "EventID\0open_vim_serial\0vim_comm\0"
+    "lock2_comm\0lock1_comm"
 };
 #undef QT_MOC_LITERAL
 
@@ -79,7 +84,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      20,   14, // methods
+      24,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -87,26 +92,30 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  114,    2, 0x08 /* Private */,
-       3,    0,  115,    2, 0x08 /* Private */,
-       4,    0,  116,    2, 0x08 /* Private */,
-       5,    0,  117,    2, 0x08 /* Private */,
-       6,    0,  118,    2, 0x08 /* Private */,
-       7,    0,  119,    2, 0x08 /* Private */,
-       8,    0,  120,    2, 0x08 /* Private */,
-       9,    0,  121,    2, 0x08 /* Private */,
-      10,    0,  122,    2, 0x08 /* Private */,
-      11,    0,  123,    2, 0x08 /* Private */,
-      12,    0,  124,    2, 0x08 /* Private */,
-      13,    0,  125,    2, 0x08 /* Private */,
-      14,    0,  126,    2, 0x08 /* Private */,
-      15,    0,  127,    2, 0x08 /* Private */,
-      16,    0,  128,    2, 0x08 /* Private */,
-      17,    0,  129,    2, 0x08 /* Private */,
-      18,    0,  130,    2, 0x08 /* Private */,
-      19,    0,  131,    2, 0x08 /* Private */,
-      20,    0,  132,    2, 0x08 /* Private */,
-      21,    1,  133,    2, 0x08 /* Private */,
+       1,    0,  134,    2, 0x08 /* Private */,
+       3,    0,  135,    2, 0x08 /* Private */,
+       4,    0,  136,    2, 0x08 /* Private */,
+       5,    0,  137,    2, 0x08 /* Private */,
+       6,    0,  138,    2, 0x08 /* Private */,
+       7,    0,  139,    2, 0x08 /* Private */,
+       8,    0,  140,    2, 0x08 /* Private */,
+       9,    0,  141,    2, 0x08 /* Private */,
+      10,    0,  142,    2, 0x08 /* Private */,
+      11,    0,  143,    2, 0x08 /* Private */,
+      12,    0,  144,    2, 0x08 /* Private */,
+      13,    0,  145,    2, 0x08 /* Private */,
+      14,    0,  146,    2, 0x08 /* Private */,
+      15,    0,  147,    2, 0x08 /* Private */,
+      16,    0,  148,    2, 0x08 /* Private */,
+      17,    0,  149,    2, 0x08 /* Private */,
+      18,    0,  150,    2, 0x08 /* Private */,
+      19,    0,  151,    2, 0x08 /* Private */,
+      20,    0,  152,    2, 0x08 /* Private */,
+      21,    1,  153,    2, 0x08 /* Private */,
+      23,    0,  156,    2, 0x08 /* Private */,
+      24,    0,  157,    2, 0x08 /* Private */,
+      25,    0,  158,    2, 0x08 /* Private */,
+      26,    0,  159,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -129,6 +138,10 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::QString,   22,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -159,6 +172,10 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 17: _t->on_pushButton_16_clicked(); break;
         case 18: _t->on_pushButton_5_clicked(); break;
         case 19: _t->LogEvent((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 20: _t->open_vim_serial(); break;
+        case 21: _t->vim_comm(); break;
+        case 22: _t->lock2_comm(); break;
+        case 23: _t->lock1_comm(); break;
         default: ;
         }
     }
@@ -189,13 +206,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 20)
+        if (_id < 24)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 20;
+        _id -= 24;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 20)
+        if (_id < 24)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 20;
+        _id -= 24;
     }
     return _id;
 }
